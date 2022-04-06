262451a76a638cbb73b52fbffc9435da57729030
mayra <mayra@192.168.0.20>
2022-04-06 08:40:35 +0200

    Hemos añadido las notas de la UF4

--- src/notas.java
+++ src/notas.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class notas {
 	//declaramos la variables que nos hacen falta
-	double uf1, uf2, uf3;
-	double acu1, acu2, acu3, def;
+	double uf1, uf2, uf3, uf4;
+	double acu1, acu2, acu3, acu4, def;
 	//utilizames ecaner para poder introducir datos
 	Scanner entrada = new Scanner(System.in);
 	
@@ -29,6 +29,10 @@ public class notas {
 		System.out.print("ingrese nota 3: ");
 		
 		uf3= entrada.nextDouble();
+		
+		System.out.print("ingrese nota 4: ");
+		
+		uf4= entrada.nextDouble();
 		
 	
 	}
@@ -57,7 +61,12 @@ public class notas {
 			System.out.println(" nota3 correcta");
 		}
 		
+		if (uf4>10) {
+			System.out.println(" nota4 mal introducida");
 			
+		}else {
+			System.out.println(" nota4 correcta");
+		}
 		
 		
 	}
@@ -65,10 +74,11 @@ public class notas {
 	
 	public void Calculonotas() {
 		acu1= uf1*0.20;
-		acu2 = uf2 * 0.35;
-		acu3 = uf3 * 0.45;
+		acu2 = uf2 * 0.20;
+		acu3 = uf3 * 0.30;
+		acu4 = uf4 * 0.10;
 		
-		def = acu1 + acu2+ acu3;
+		def = acu1 + acu2+ acu3+ acu4;
 		
 		//hasta aqui la tenemos calculada peor no la mostramos
 	}
@@ -78,10 +88,12 @@ public class notas {
 		System.out.println(" nota1 = " + uf1);
 		System.out.println(" nota2 = " + uf2);
 		System.out.println(" nota3 = " + uf3);
+		System.out.println(" nota3 = " + uf4);
 		
 		System.out.println(" acumuado 1 = "+ acu1);
 		System.out.println(" acumuado 2 = "+ acu2);
 		System.out.println(" acumuado 3 = "+ acu3);
+		System.out.println(" acumuado 3 = "+ acu4);
 		
 		System.out.println(" nota definitiva es = "+ def);
 		
